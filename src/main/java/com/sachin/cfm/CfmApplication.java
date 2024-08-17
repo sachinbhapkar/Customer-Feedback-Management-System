1a54050df860d249ae04840ce07926ba7819cac9
sachinbhapkar <bhapkarsr@gmail.com>
2024-08-17 13:26:03 +0530

    first commit

---
+++ src/main/java/com/sachin/cfm/CfmApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.cfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachin/cfm/CfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.cfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
